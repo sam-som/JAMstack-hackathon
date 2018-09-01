0a6393a3dd50957efd5fe2c1c2e352411c2911a0
Sam Som <sams@cacpqua-samsom.local>
2018-08-31 17:27:19 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi FreeCodeCamp</h1>
+    <p>I'm currently a QA Engineer who consider transition into Software Engineer role</p>
+    <p>I would love to be able to attend the in person hackathon to meet people and build a solution to a problem</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
